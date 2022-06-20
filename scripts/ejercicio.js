a204d05a5d68c8d18a9fa0b8a71968a6a45c0ba9
Brayan Betancourt Ramirez <67015417+Betancourt1996@users.noreply.github.com>
2022-06-20 15:27:44 -0500

    fotos

--- scripts/ejercicio.js
+++ scripts/ejercicio.js
@@ -9,5 +9,60 @@ let ejecutarCodigo = () => {
     arreglo =document.getElementsByTagName('p');
     console.log(arreglo[2]);
     arreglo[2].setAttribute('class','d-none');
+    let imgs = [
+        {
+          url: "https://images.unsplash.com/photo-1578255321055-d9ed6a976af5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "perro",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1653938245506-3203e63338b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "surf",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1648737966282-b193f74a1503?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+          alt: "papá",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "programar",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1653934004892-1399fb4444c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "nieve",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1653242816429-fd9393b49bb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "tazas",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1653904780118-96d8b4b1eced?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNzZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "globo",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1653919523364-1e7e221d2917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          alt: "moto",
+        },
+        {
+          url: "https://images.unsplash.com/photo-1654032693719-ada7186b1ae7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+          alt: "azul",
+        },
+      ];
+      
+      const arr_svg =
+        document.getElementsByClassName("bd-placeholder-img card-img-top");
+      
+      for (let i = 0; arr_svg.length && i < imgs.length; i++) {
+        const img = document.createElement("img");
+        img.src = imgs[i].url;
+        img.alt = imgs[i].alt;
+      
+        arr_svg[0].replaceWith(img);
+      }
+
 }
 ejecutarCodigo();
+
+
+
+
+
